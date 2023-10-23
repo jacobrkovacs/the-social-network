0c52ac3f955177dbb01a392ca20888a2c534fa02
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-10-23 14:54:26 -0700

    adds user schema and installs validator

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const {Schema, model} = require('mongoose');
+const validator = require('validator')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true,
+
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email is required',
+            validate: [validator.isEmail, 'Invalid Email']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts',
+            }
+        ],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema
+.virtual('friendCount')
+.get(() => {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
