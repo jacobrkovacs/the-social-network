fbd5bf5f523d910222a0feb7613cb0b3358acca9
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-10-25 13:39:30 -0700

    adds index for models. Finishes Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -4,11 +4,25 @@ const {Schema, model, now} = require('mongoose');
 const reactionSchema = Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    }
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: 'user'
+    },
+    createdAt: { 
+        type: Date,
+        default: Date.now
+    },
 })
 
-const thoughtSchema = Schema(
+const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
@@ -26,5 +40,21 @@ const thoughtSchema = Schema(
             ref: 'user'
         },
          reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
     }
-)
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(() => {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought
